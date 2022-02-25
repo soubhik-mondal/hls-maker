486bad770649d90fe26420a80e03f70f5a8ed556
Soubhik Mondal <souvikmondal.j@gmail.com>
2022-02-25 08:07:39 +0530

    Fix webhook bug

--- src/webhook.js
+++ src/webhook.js
@@ -2,11 +2,11 @@
 
 const fetch = require('node-fetch');
 
-module.exports = function (url, method) {
-	const request = async body => {
-		const response = await fetch(url, {
-			method,
-			body: JSON.stringify(body),
+module.exports = function (webhookUrl, webhookMethod) {
+	const request = async videoUrl => {
+		const response = await fetch(webhookUrl, {
+			method: webhookMethod,
+			body: JSON.stringify({ url: videoUrl }),
 			headers: { 'Content-Type': 'application/json' },
 		});
 		return await response.text();
